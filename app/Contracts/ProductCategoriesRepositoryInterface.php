a77e95e0fe1e60f4c9ba70f4013986dcc569c179
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-08-06 18:56:43 -0400

    Repositorio para Categoria de produtos

---
+++ app/Contracts/ProductCategoriesRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Contracts;
+
+interface ProductCategoriesRepositoryInterface extends BaseRepositoryInterface
+{
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contracts\ProductCategoriesRepositoryInterface;
+use App\Repositories\ProductCategoriesRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ProductCategoriesRepositoryInterface::class, ProductCategoriesRepository::class);
     }
 
     /**
