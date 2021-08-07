6d62efef4a004f1a9334ad6eb84612ce1605d331
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-08-07 12:40:38 -0400

    Criando model de produto

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = [
+        'product_category_id',
+        'registration_date',
+        'product_name',
+        'product_value'
+    ];
+
+    protected $casts = [
+        'product_value' => 'float'
+    ];
+
+    public function productCategory()
+    {
+        return $this->hasOne(ProductCategory::class, 'product_category_id', 'id');
+    }
+
+    public function scopeProductCategoryName($query)
+    {
+        return $query->addSelect([
+            'product_category_name' => ProductCategory::select('name_category')
+                ->whereColumn('product_categories.id', 'products.product_category_id')
+                ->limit(1)
+        ]);
+    }
+
+}
