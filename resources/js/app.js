169cb7cb0959beea781e1bc1e5321b39a69d22d5
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-08-07 09:47:02 -0400

    Configurando Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,14 @@ require('./bootstrap');
 
 import Vue from "vue";
 
+import Axios from "axios";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.use(VueSweetalert2);
+
+Vue.prototype.$axios = Axios;
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -23,6 +27,36 @@ Vue.use(VueSweetalert2);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Axios.interceptors.response.use(
+    response => response,
+    error => {
+        const { response } = error;
+
+        if (response && response.status === 500) {
+            Vue.prototype.$swal.fire(
+                "Erro",
+                "Oops! Algo deu errado.",
+                "error"
+            );
+        }
+        if (response && response.status === 422) {
+            let message = '';
+            Object.keys(response.data.errors).forEach(item => {
+                let field = response.data.errors[item];
+                for (let i = 0; i < field.length; i++) {
+                    message += field[i] + "<br />";
+                }
+            });
+
+            Vue.prototype.$swal.fire(
+                "Erro",
+                message,
+                "error"
+            );
+        }
+        return Promise.reject(error);
+    }
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
