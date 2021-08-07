f2c56b66902a2d56a8a1a5228598b08ac74f14ca
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-08-07 10:43:39 -0400

    Criando testes para categorias de produtos

---
+++ tests/Feature/ProductCategoriesTest.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ProductCategories;
+use App\Models\User;
+use Tests\TestCase;
+
+class ProductCategoriesTest extends TestCase
+{
+    /**
+     * Listando todas as categorias
+     *
+     * @return void
+     * @test
+     */
+    public function listAllProductCategories()
+    {
+        $this->actingAs(User::find(1));
+
+        $response = $this->getJson('/api/product-categories');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Criando uma nova categoria
+     *
+     * @return void
+     * @test
+     */
+    public function createProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $categories = factory(ProductCategories::class)->make();
+
+        $response = $this->postJson('/api/product-categories', [
+            'name_category' => $categories->name_category,
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'name' => $categories->name_category,
+        ]);
+    }
+
+    /**
+     * Falhando ao criar uma nova categoria
+     *
+     * @return void
+     * @test
+     */
+    public function failCreateProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $response = $this->postJson('/api/product-categories', [
+            'name_category' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Atualizando uma categoria
+     *
+     * @return void
+     * @test
+     */
+    public function updateProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $categories = factory(ProductCategories::class)->create();
+
+        $response = $this->putJson('/api/product-categories/'. $categories->id, [
+            'name_category' => $categories->name_category . 'Atualizado',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'name' => $categories->name_category . 'Atualizado',
+        ]);
+    }
+
+    /**
+     * Falhar ao atualizar uma categoria
+     *
+     * @return void
+     * @test
+     */
+    public function failUpdateProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $categories = factory(ProductCategories::class)->create();
+
+        $response = $this->putJson('/api/product-categories/'. $categories->id, [
+            'name_category' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Falhar ao atualizar uma categoria inexistente
+     *
+     * @return void
+     * @test
+     */
+    public function failUpdateProductCategoryNoExistent()
+    {
+        $this->actingAs(User::find(1));
+
+        $response = $this->putJson('/api/product-categories/'. 200000, [
+            'name_category' => 'teste',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Excluindo uma categoria
+     *
+     * @return void
+     * @test
+     */
+    public function destroyProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $categories = factory(ProductCategories::class)->create();
+
+        $response = $this->deleteJson('/api/product-categories/'. $categories->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Falhar ao Excluir uma categoria
+     *
+     * @return void
+     * @test
+     */
+    public function failDestroyProductCategory()
+    {
+        $this->actingAs(User::find(1));
+
+        $response = $this->deleteJson('/api/product-categories/'. 20000);
+
+        $response->assertStatus(200);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected static $setUpHasRunOnce = false;
+
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if(!static::$setUpHasRunOnce) {
+
+            $this->artisan('config:cache --env=testing');
+            $this->artisan('migrate:fresh', ['-vvv' => true]);
+            $this->artisan('db:seed', ['-vvv' => true]);
+
+            static::$setUpHasRunOnce = true;
+        }
+    }
 }
