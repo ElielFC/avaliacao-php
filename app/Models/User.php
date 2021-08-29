1d8bc745fdd9fb1115fd0aeb8da772d3438449e5
Eliel Canivarolli <eliel.f.canivarolli@gmail.com>
2021-08-29 09:30:16 -0400

    Comfigurando log nas models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     use Notifiable;
 
     /**
